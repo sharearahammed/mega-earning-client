bc30c203f95ef5e5cdc0dbadde4f644def4fa78d
Sharear Ahammed <sharearahammed@gmail.com>
2024-06-01 19:06:31 +0600

    Features section

--- src/assets/Components/Home/Features/Features.jsx
+++ src/assets/Components/Home/Features/Features.jsx
@@ -8,7 +8,7 @@ const Features = () => {
       <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
         <h2 className="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
           Features
-        </h2>
+        </h2> 
 
         <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
           The product can personalize user experiences by understanding
