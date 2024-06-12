e364eb67c35207d5792e00d79d417b6e9020fba5
Sharear Ahammed <sharearahammed@gmail.com>
2024-06-12 23:38:09 +0600

    update pagination

--- src/assets/Components/Dashboard/WorkerDashBoard/WorkerTaskList.jsx
+++ src/assets/Components/Dashboard/WorkerDashBoard/WorkerTaskList.jsx
@@ -1,20 +1,50 @@
-import { useQuery } from "@tanstack/react-query";
 import WorkerTaskListCard from "./WorkerTaskListCard";
 import useAxiosSecure from "../../Hook/useAxiosSecure";
 import { Helmet } from "react-helmet-async";
+import { useEffect, useState } from "react";
 
 const WorkerTaskList = () => {
 
     const axiosSecure = useAxiosSecure();
-    const {
-        data: tasks = []
-      } = useQuery({
-        queryKey: ['tasks'],
-        queryFn: async () => {
-          const res = await axiosSecure.get(`/tasks`)
-          return res.data
-        },
-      })
+    const itemsPerPage = 6
+    const [currentPage, setCurrentPage] = useState(1);
+    const [count, setCount] = useState(0);
+    const [alltasks, setAlltasks] = useState([]);
+    
+    useEffect(() => {
+      const getData = async () => {
+        try {
+          const { data } = await axiosSecure.get(`/tasks?page=${currentPage}&size=${itemsPerPage}`);
+          setAlltasks(data);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+      getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [itemsPerPage,currentPage]);
+
+    useEffect(() => {
+      const getData = async () => {
+        try {
+          const { data } = await axiosSecure.get(`/taskss`);
+          setCount(data.result.length);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+      getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+
+      const numberOfPages = Math.ceil(count / itemsPerPage);
+
+      const pages = [...Array(numberOfPages).keys()].map((e) => e + 1);
+      // handle pagination
+      const handlePaginationButton = (value) => {
+        setCurrentPage(value)
+      };
       
 
     return (
@@ -32,12 +62,70 @@ const WorkerTaskList = () => {
     {/* maping */}
     <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 text-sm sm:mt-20 sm:grid-cols-2 md:gap-y-10 lg:max-w-none lg:grid-cols-2">
     {
-        tasks?.map(task=><WorkerTaskListCard key={task._id}
+        alltasks?.map(task=><WorkerTaskListCard key={task._id}
             task={task}
              />)
     }
     </div>
-    
+    <div className="flex justify-center mt-12">
+        <button
+        disabled={currentPage  === 1}
+        onClick={()=>handlePaginationButton(currentPage-1)}
+         className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white">
+          <div className="flex items-center -mx-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6 mx-1 rtl:-scale-x-100"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M7 16l-4-4m0 0l4-4m-4 4h18"
+              />
+            </svg>
+
+            <span className="mx-1">previous</span>
+          </div>
+        </button>
+
+        {pages.map((btnNum) => (
+          <button
+            onClick={() => handlePaginationButton(btnNum)}
+            key={btnNum}
+            className={`hidden ${currentPage === btnNum ? "bg-[#22AB59]" : ""} px-4 py-2 mx-1 transition-colors duration-300 text-gray-800 transform  rounded-md sm:inline hover:bg-gray-300  hover:text-black`}
+          >
+            {btnNum}
+          </button>
+        ))}
+
+        <button
+        disabled={currentPage === numberOfPages}
+        onClick={()=>handlePaginationButton(currentPage+1)}
+        className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500">
+          <div className="flex items-center -mx-1">
+            <span className="mx-1">Next</span>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6 mx-1 rtl:-scale-x-100"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              />
+            </svg>
+          </div>
+        </button>
+      </div>
   </div>
 </section>
     );
