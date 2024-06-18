9898242830b54e5eb48a3ae5f59d3c83e4bc6018
Sharear Ahammed <sharearahammed@gmail.com>
2024-06-18 17:43:38 +0600

    update banner

--- src/assets/Components/Home/Banner/Banner.jsx
+++ src/assets/Components/Home/Banner/Banner.jsx
@@ -1,6 +1,7 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { Link } from "react-router-dom";
+import Typewriter from "typewriter-effect";
 
 const Banner = () => {
   return (
@@ -9,63 +10,92 @@ const Banner = () => {
         <div
           className="mt-[65px] lg:mt-[85px] hero lg:min-h-screen lg:bg-no-repeat lg:bg-cover h-[400px]"
           style={{
-            backgroundImage:
-              'url("https://i.ibb.co/QPpXp1C/earn-money01.jpg")',
+            backgroundImage: 'url("https://i.ibb.co/QPpXp1C/earn-money01.jpg")',
           }}
         >
           <div className="bg-black h-full w-full bg-opacity-55 flex flex-col items-center justify-center text-white">
             <div className="pl-5 md:pl-20 lg:pl-20 w-full text-left flex flex-col justify-start">
-            <h1 className="text-white lg:mb-10 pb-5 font-bold text-xl lg:text-[60px]">
-            Earn Cash for Simple Tasks!
-            </h1>
-            <p className="lg:text-[25px] font-extralight pb-5">
-            Complete tasks, earn money, and watch your earnings grow
-            </p>
-            <Link to={"/login"}>
-            <button className="transition-transform duration-300 ease-in-out hover:scale-110 bg-gradient-to-r from-emerald-500 from-10% via-green-500 via-50% to-green-600 to-90% lg:text-[20px] text-[10px] px-7 py-2 rounded-full mt-5">Start Earning Now</button>
-            </Link>
+              <h1 className="text-white lg:mb-10 pb-5 font-bold text-xl lg:text-[60px]">
+                <Typewriter
+                  options={{
+                    strings: ["Earn Cash for Simple Tasks!"],
+                    autoStart: true,
+                    loop: true,
+                    delay: 70,
+                    deleteSpeed: 40,
+                  }}
+                />
+              </h1>
+              <p className="lg:text-[25px] font-extralight pb-5">
+                Complete tasks, earn money, and watch your earnings grow
+              </p>
+              <Link to={"/login"}>
+                <button className="transition-transform duration-300 ease-in-out hover:scale-110 bg-gradient-to-r from-emerald-500 from-10% via-green-500 via-50% to-green-600 to-90% lg:text-[20px] text-[10px] px-7 py-2 rounded-full mt-5">
+                  Start Earning Now
+                </button>
+              </Link>
             </div>
           </div>
         </div>
         <div
           className="mt-[65px] lg:mt-[85px] hero lg:min-h-screen lg:bg-no-repeat lg:bg-cover h-[400px]"
           style={{
-            backgroundImage:
-              'url("https://i.ibb.co/CnhWCBt/flexible.png")',
+            backgroundImage: 'url("https://i.ibb.co/CnhWCBt/flexible.png")',
           }}
         >
           <div className="bg-black h-full w-full bg-opacity-55 flex flex-col items-center justify-center text-white">
             <div className="pl-5 md:pl-20 lg:pl-20 w-full text-left flex flex-col justify-start">
-            <h1 className="text-white lg:mb-10 pb-5 font-bold text-xl lg:text-[60px]">
-            Work Anytime, Anywhere
-            </h1>
-            <p className="lg:text-[25px] pb-5">
-            Enjoy the freedom to work on your own schedule.
-            </p>
-            <Link to={"/login"}>
-            <button className="bg-gradient-to-r from-emerald-500 from-10% via-green-500 via-50% to-green-600 to-90% lg:text-[20px] text-[10px] px-7 py-2 rounded-full mt-5 transition-transform duration-300 ease-in-out hover:scale-110">Get Started</button>
-            </Link>
+              <h1 className="text-white lg:mb-10 pb-5 font-bold text-xl lg:text-[60px]">
+              <Typewriter
+                  options={{
+                    strings: ["Work Anytime, Anywhere"],
+                    autoStart: true,
+                    loop: true,
+                    delay: 70,
+                    deleteSpeed: 40,
+                  }}
+                />
+                
+              </h1>
+              <p className="lg:text-[25px] pb-5">
+                Enjoy the freedom to work on your own schedule.
+              </p>
+              <Link to={"/login"}>
+                <button className="bg-gradient-to-r from-emerald-500 from-10% via-green-500 via-50% to-green-600 to-90% lg:text-[20px] text-[10px] px-7 py-2 rounded-full mt-5 transition-transform duration-300 ease-in-out hover:scale-110">
+                  Get Started
+                </button>
+              </Link>
             </div>
           </div>
         </div>
         <div
           className="mt-[65px] lg:mt-[85px] hero lg:min-h-screen lg:bg-no-repeat lg:bg-cover h-[400px]"
           style={{
-            backgroundImage:
-              'url("https://i.ibb.co/jrjPTx7/success.jpg")',
+            backgroundImage: 'url("https://i.ibb.co/jrjPTx7/success.jpg")',
           }}
         >
           <div className="bg-black h-full w-full bg-opacity-55 flex flex-col items-center justify-center text-white">
             <div className="pl-5 md:pl-20 lg:pl-20 w-full text-left flex flex-col justify-start">
-            <h1 className="text-white lg:mb-10 pb-5 font-bold text-xl lg:text-[60px]">
-            Success Stories from Our Users
-            </h1>
-            <p className="lg:text-[25px] pb-5">
-            See how our platform is changing lives
-            </p>
-            <Link to={"/login"}>
-            <button className="bg-gradient-to-r from-emerald-500 from-10% via-green-500 via-50% to-green-600 to-90% lg:text-[20px] text-[10px] px-7 py-2 rounded-full mt-5 transition-transform duration-300 ease-in-out hover:scale-110">Join Our Community</button>
-            </Link>
+              <h1 className="text-white lg:mb-10 pb-5 font-bold text-xl lg:text-[60px]">
+              <Typewriter
+                  options={{
+                    strings: ["Success Stories from Our Users"],
+                    autoStart: true,
+                    loop: true,
+                    delay: 70,
+                    deleteSpeed: 40,
+                  }}
+                />
+                
+              </h1>
+              <p className="lg:text-[25px] pb-5">
+                See how our platform is changing lives
+              </p>
+              <Link to={"/login"}>
+                <button className="bg-gradient-to-r from-emerald-500 from-10% via-green-500 via-50% to-green-600 to-90% lg:text-[20px] text-[10px] px-7 py-2 rounded-full mt-5 transition-transform duration-300 ease-in-out hover:scale-110">
+                  Join Our Community
+                </button>
+              </Link>
             </div>
           </div>
         </div>
